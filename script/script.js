4009bac7eba3d15ec6dae9bcfafc34a65d391f9c
Rynare <106422092+Rynare@users.noreply.github.com>
2022-06-14 22:30:18 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,19 +1,3 @@
-const arra = ["short_article", "about_me", "content", "contact"]
-
-arra.forEach((item, nomer) => {
-	var ganti = document.getElementsByClassName(item)[0]
-	fetch("./html/" + item + ".html").then(raw => raw.text()).then(ready => ganti.innerHTML = ready)
-	if (nomer == (arra.length - 1)) {
-		loadScript()
-	}
-})
-
-function loadScript() {
-	let content_src = document.createElement("script")
-	content_src.src = "script/img-slider.js"
-	document.body.appendChild(content_src)
-}
-
 function toTheTop() {
 	location.href = window.location.href;
 }
