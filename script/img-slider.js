8585e14f583cb125508eac8006a359fd95131274
Rynare <106422092+Rynare@users.noreply.github.com>
2022-06-16 10:29:21 +0700

    Add files via upload

--- script/img-slider.js
+++ script/img-slider.js
@@ -1,4 +1,4 @@
-// Place in header (do not use async or defer)
+// Place in header(do not use async or defer)
 document.addEventListener('readystatechange', event => {
     switch (document.readyState) {
         case "loading":
@@ -21,30 +21,10 @@ document.addEventListener('readystatechange', event => {
             break;
     }
 });
-var index = 0;
-showImage(index)
 
-function showImage(i) {
-    index += i;
-    let images = document.getElementsByClassName("anime_image")
-    // var dots = document.getElementsByClassName("anime_dot");
-    // set all images to display hidden / none
-    for (i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
+function autoGrow(element) {
+    if (element.scrollHeight <= 250) {
+        element.style.height = "5px";
+        element.style.height = (element.scrollHeight) + "px";
     }
-
-    // for (i = 0; i < dots.length; i++) {
-    // 	dots[i].className = dots[i].className.replace(" active", "");
-    // }
-    // // back to 1st image if user click next on last image
-    if (index > images.length - 1) {
-        index = 0;
-    }
-    // back to last image if user click prev on 1st image
-    if (index < 0) {
-        index = images.length - 1;
-    }
-    // display block only specific image after all image being hidden/none. but except the specific image they are still hidden/none
-    images[index].style.display = "block";
-    // dots[index].className += " active";
 }
--- script/script.js
+++ script/script.js
@@ -1,10 +1,42 @@
+var ke_n = 0
+
+running()
+
+function running() {
+	const arra = ["short_article", "about_me", "content", "contact"]
+	arra.forEach(item => ambilResource(item))
+}
+
+function ambilResource(source) {
+	var ganti = document.getElementsByClassName(source)[0]
+	fetch("./html/" + source + ".html").then(response => response.text()).then(response => ganti.innerHTML = response)
+}
+
 function toTheTop() {
 	location.href = window.location.href;
 }
 
-function autoGrow(element) {
-	if (element.scrollHeight <= 250) {
-		element.style.height = "5px";
-		element.style.height = (element.scrollHeight) + "px";
+function showImage(i) {
+	ke_n += i;
+	let images = document.getElementsByClassName("anime_image")
+	// var dots = document.getElementsByClassName("anime_dot");
+	// set all images to display hidden / none
+	for (i = 0; i < images.length; i++) {
+		images[i].style.display = "none";
 	}
+
+	// for (i = 0; i < dots.length; i++) {
+	// 	dots[i].className = dots[i].className.replace(" active", "");
+	// }
+	// // back to 1st image if user click next on last image
+	if (ke_n > images.length - 1) {
+		ke_n = 0;
+	}
+	// back to last image if user click prev on 1st image
+	if (ke_n < 0) {
+		ke_n = images.length - 1;
+	}
+	// display block only specific image after all image being hidden/none. but except the specific image they are still hidden/none
+	images[ke_n].style.display = "block";
+	// dots[ke_n].className += " active";
 }
